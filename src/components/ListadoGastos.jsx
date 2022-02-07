902be76f26e3542ef0bad8f39402d3c423ea8608
Gabriel De Los Rios <gab.delosrios@gmail.com>
2022-02-07 19:03:03 -0300

    Fixed little bug on ListadoGastos component

--- src/components/ListadoGastos.jsx
+++ src/components/ListadoGastos.jsx
@@ -23,7 +23,7 @@ const ListadoGastos = ({
                     
                     ) :  (
                     <>
-                        <h2>{gastosFiltrados.length ? 'Gastos' : 'No hay gastos aun'}</h2>
+                        <h2>{gastos.length ? 'Gastos' : 'No hay gastos aun'}</h2>
                        { gastos.map(gasto => <Gasto 
                             gasto={gasto} 
                             key={gasto.id} 
